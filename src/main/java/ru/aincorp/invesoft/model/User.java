d7acf57824e0cebb66f04649f439b6ea05cfe182
Christina Ivanova <icha888k@gmail.com>
2018-08-13 21:18:37 +0300

    InventorySoft 3 Создание класса User

---
+++ src/main/java/ru/aincorp/invesoft/model/User.java
@@ -0,0 +1,33 @@
+package ru.aincorp.invesoft.model;
+
+public class User {
+    private String firstName;
+    private String lastName;
+
+    public User() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+
+    public String getLastName() {
+
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
